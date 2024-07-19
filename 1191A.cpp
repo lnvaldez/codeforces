81dffdd59c104ca253ea97d2f1cc328ae88f58df
Lucas Valdez <lnvaldez24@gmail.com>
2024-07-19 10:23:05 -0400

    Create 1191A.cpp

---
+++ 1191A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define CASE_A (x = 1)
+#define CASE_B (x = 3)
+#define CASE_C (x = 2)
+#define CASE_D (x = 0)
+
+int main(){
+	
+	int x, a, t; 
+	cin >> x;
+	
+	cout << x % 4;
+		
+	
+	return 0;
+}
+
